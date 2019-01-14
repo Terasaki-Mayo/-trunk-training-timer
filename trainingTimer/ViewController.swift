3b20b7dc790eaeffb939f90b4bd052b95c3282af
maybou <uminokaze521@gmail.com>
2019-01-14 18:17:50 +0900

    show table view

--- trainingTimer/ViewController.swift
+++ trainingTimer/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+let trainingConfig = ["トレーニング時間", "休憩時間", "セット数"]
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +20,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return trainingConfig.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルを取得する
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        // セルに表示する値を設定する
+        cell.textLabel!.text = trainingConfig[indexPath.row]
+        return cell
+    }
+    
+
 
 
 }
