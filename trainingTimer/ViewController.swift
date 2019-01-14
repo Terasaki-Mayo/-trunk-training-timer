079f116c5470ee5ea1b434237a9b9e141b871354
maybou <uminokaze521@gmail.com>
2019-01-14 13:26:36 +0900

    Initial Commit

---
+++ trainingTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trainingTimer
+//
+//  Created by 寺崎麻代 on 2019/01/14.
+//  Copyright © 2019年 寺崎麻代. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
